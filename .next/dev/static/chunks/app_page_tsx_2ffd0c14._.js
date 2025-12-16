8607cc38ffb7a3fc66c4efee083c5a94aa8e4ce3
3mcode-solutions <admin@3mcode-solutions.com>
2025-12-16 03:55:48 +0200

    build: Generate Next.js development artifacts and update environment type definitions.

--- .next/dev/static/chunks/app_page_tsx_2ffd0c14._.js
+++ .next/dev/static/chunks/app_page_tsx_2ffd0c14._.js
@@ -1,8 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_179ddc17._.js",
-  "static/chunks/node_modules__pnpm_da32cf1a._.js"
+  "static/chunks/_3ca17d1a._.js",
+  "static/chunks/77b74_axios_lib_3ffa8d91._.js",
+  "static/chunks/node_modules__pnpm_638449e4._.js"
 ],
     source: "dynamic"
 });
